13fa79c963e7c91f84dfbd1b20fbe334f567a300
asfrod <andresperez192@gmail.com>
2022-07-15 12:11:19 -0500

    Functional dimension button

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
-//Llamando mi div para crear el grid
-const grids = document.querySelector(".grids");
+//Initializing the function to create the grid when the webpage load
+createGrid()
 
-//Creating a loop to iterate in a matrix 16*16
-let i =1;
-let j = 1;
-for (i = 1; i<=16; i++) {
-    const divI = document.createElement('div');
-    divI.classList.add(`row${i}`, 'rows');
-    grids.appendChild(divI);
+//Function to create a grid for the Etch a Sketch and his respective
+//hover event 
+function createGrid(dimen = 16) {
+    //Selecting the section of the document (div element) in which create the EaS
+    const grids = document.querySelector(".grids");
+
+    //Creating a div element to contain the Etch a Sketch
+    const etchSketch = document.createElement('div');
+    etchSketch.classList.add('eAs');
     
-    for (j = 1; j<=16; j++) {
-        const divJ = document.createElement('div');
-        divJ.classList.add(`${i},${j}`, 'grid');
-        divI.appendChild(divJ);
+    //Creating a loop to iterate in a matrix dimen * dimen
+     for (let i = 1; i<=dimen; i++) {
+        const divI = document.createElement('div');
+        divI.classList.add(`row${i}`, 'rows');
+        etchSketch.appendChild(divI);
+        
+        for (let j = 1; j<=dimen; j++) {
+            const divJ = document.createElement('div');
+            divJ.classList.add(`${i},${j}`, 'grid');
+            divI.appendChild(divJ);
+        }
     }
+    grids.appendChild(etchSketch);
+
+    //Creating a hover event and adding a new class with another background 
+    //color in css
+    const grid = Array.from(document.querySelectorAll('.grid'));
+    grid.forEach(g => g.addEventListener('mouseover', () => {
+        g.classList.add('classic');
+    }));
+    
+    //creating a click event to reset to the original class (and original color)
+    const reset = document.querySelector('.reset');
+    reset.addEventListener('click', () => {
+        grid.forEach(g => g.classList.remove('classic'));
+    });
+
 }
 
-//Creating a hover event and adding a new class with another background color in css
-const grid = Array.from(document.querySelectorAll('.grid'));
-grid.forEach(g => g.addEventListener('mouseover', () => {
-    g.classList.add('classic');
-}));
-
-
-//creating a click event to reset to the original class (and original color)
-const reset = document.querySelector('.reset');
-reset.addEventListener('click', () => {
-    grid.forEach(g => g.classList.remove('classic'));
-});
-
-
-
-
-//Prompt to specify dimensions 
-let a = 16;
+//Prompt a specify dimension
 const dim = document.querySelector('.dim');
 dim.addEventListener('click', () => {
-    a = prompt('Ingrese las dimensiones');
-    console.log(a);
+    let a = prompt('Ingrese las dimensiones');
+    const eAs = document.querySelector('.eAs')
+    eAs.remove();
+    createGrid(a);
 })
-
-
-
-
